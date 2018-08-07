6da23dfcb9e4f23e0d64429e9d3ff7eb64366240
Vashali <vaishali2016@outlook.com>
2018-08-07 14:07:28 +0100

    Task complete

--- handler.js
+++ handler.js
@@ -1,12 +1,31 @@
 const express=require('express')
 const app = express()
 
-console.log(new Date())
 
-app.get('/api/timestamp/:date_string',function(req, res) {
- const date=new Date((req.params.date_string))
- const jsonDate = {"unix":date.getTime(), "utc":date.toUTCString()}
- res.json(jsonDate)
+
+app.get('/api/timestamp/:date_string?',function(req, res) {
+
+
+
+
+ 
+ let date=new Date()
+
+ if(req.params.date_string){
+ 	date=new Date((req.params.date_string))
+ }
+
+ if(date!="Invalid Date"){
+ 	const jsonDate = {"unix":date.getTime(), "utc":date.toUTCString()}
+
+ 	res.json(jsonDate)
+ }else{
+ 	const invalidJson = {"error":"Invalid Date"}
+ 	res.json(invalidJson)
+
+ }
+
+ 
 })
 
 app.listen(8080, function() {
